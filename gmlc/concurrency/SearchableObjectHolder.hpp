dd8280f2bca17c37a91ca967615989905755faec
Philip Top <phlptp@gmail.com>
2019-08-20 20:38:24 -0700

    update google test and add some functionality to the searchable Object Holder

--- gmlc/concurrency/SearchableObjectHolder.hpp
+++ gmlc/concurrency/SearchableObjectHolder.hpp
@@ -16,6 +16,7 @@ All rights reserved. SPDX-License-Identifier: BSD-3-Clause
 #include <memory>
 #include <mutex>
 #include <thread>
+#include <vector>
 
 namespace gmlc
 {
@@ -28,7 +29,7 @@ class SearchableObjectHolder
 {
   private:
     std::mutex mapLock;
-    std::map<std::string, std::shared_ptr<X>> ObjectMap;
+    std::map<std::string, std::shared_ptr<X>> objectMap;
 #ifdef ENABLE_TRIPWIRE
     TripWireDetector trippedDetect;
 #endif
@@ -49,7 +50,7 @@ class SearchableObjectHolder
 #endif
         std::unique_lock<std::mutex> lock(mapLock);
         int cntr = 0;
-        while (!ObjectMap.empty())
+        while (!objectMap.empty())
         {
             ++cntr;
             lock.unlock();
@@ -70,33 +71,58 @@ class SearchableObjectHolder
             }
         }
     }
+    /** add and object to container*/
     bool addObject(const std::string &name, std::shared_ptr<X> obj)
     {
         std::lock_guard<std::mutex> lock(mapLock);
-        auto res = ObjectMap.emplace(name, std::move(obj));
+        auto res = objectMap.emplace(name, std::move(obj));
         return res.second;
     }
+    /** check if the container is empty
+    @details this is really only useful if there is only one thread adding
+    object otherwise the results are not totally reliable upon return
+    */
+    bool empty()
+    {
+        std::lock_guard<std::mutex> lock(mapLock);
+        return objectMap.empty();
+    }
 
+    /** get a vector of all the contained objects*/
+    std::vector<std::shared_ptr<X>> getObjects()
+    {
+        std::vector<std::shared_ptr<X>> objs;
+        std::lock_guard<std::mutex> lock(mapLock);
+        for (auto &obj : objectMap)
+        {
+            objs.push_back(obj.second);
+        }
+        return objs;
+    }
+
+    /** remove an object from the object holder by name*/
     bool removeObject(const std::string &name)
     {
         std::lock_guard<std::mutex> lock(mapLock);
-        auto fnd = ObjectMap.find(name);
-        if (fnd != ObjectMap.end())
+        auto fnd = objectMap.find(name);
+        if (fnd != objectMap.end())
         {
-            ObjectMap.erase(fnd);
+            objectMap.erase(fnd);
             return true;
         }
         return false;
     }
 
+    /** remove an object if it matches a certain criteria as given by the
+     * function operator*/
     bool removeObject(std::function<bool(const std::shared_ptr<X> &)> operand)
     {
         std::lock_guard<std::mutex> lock(mapLock);
-        for (auto obj = ObjectMap.begin(); obj != ObjectMap.end(); ++obj)
+        for (auto obj = objectMap.begin(); obj != objectMap.end(); ++obj)
         {
             if (operand(obj->second))
             {
-                ObjectMap.erase(obj);
+                objectMap.erase(obj);
                 return true;
             }
         }
@@ -107,11 +133,11 @@ class SearchableObjectHolder
                     const std::string &copyToName)
     {
         std::lock_guard<std::mutex> lock(mapLock);
-        auto fnd = ObjectMap.find(copyFromName);
-        if (fnd != ObjectMap.end())
+        auto fnd = objectMap.find(copyFromName);
+        if (fnd != objectMap.end())
         {
             auto newObjectPtr = fnd->second;
-            auto ret = ObjectMap.emplace(copyToName, std::move(newObjectPtr));
+            auto ret = objectMap.emplace(copyToName, std::move(newObjectPtr));
             return ret.second;
         }
         return false;
@@ -126,8 +152,8 @@ class SearchableObjectHolder
         }
 #endif
         std::lock_guard<std::mutex> lock(mapLock);
-        auto fnd = ObjectMap.find(name);
-        if (fnd != ObjectMap.end())
+        auto fnd = objectMap.find(name);
+        if (fnd != objectMap.end())
         {
             return fnd->second;
         }
@@ -139,9 +165,9 @@ class SearchableObjectHolder
     {
         std::lock_guard<std::mutex> lock(mapLock);
         auto obj =
-          std::find_if(ObjectMap.begin(), ObjectMap.end(),
+          std::find_if(objectMap.begin(), objectMap.end(),
                        [&operand](auto &val) { return operand(val.second); });
-        if (obj != ObjectMap.end())
+        if (obj != objectMap.end())
         {
             return obj->second;
         }
--- tests/SearchableObjectHolderTests.cpp
+++ tests/SearchableObjectHolderTests.cpp
@@ -34,3 +34,19 @@ TEST(SOH_tests, basic_tests)
     auto res2 = SOH1.findObject("test1");
     EXPECT_FALSE(res2);
 }
+
+TEST(SOH_tests, contained_tests)
+{
+    SearchableObjectHolder<std::string> SOH1;
+    EXPECT_TRUE(SOH1.empty());
+    SOH1.addObject("test1", std::make_shared<std::string>("test_1"));
+    SOH1.addObject("test2", std::make_shared<std::string>("test_2"));
+    SOH1.addObject("test3", std::make_shared<std::string>("test_3"));
+
+    auto v = SOH1.getObjects();
+    ASSERT_EQ(v.size(), 3u);
+    EXPECT_EQ(*(v[0]), "test_1");
+    EXPECT_EQ(*(v[1]), "test_2");
+    EXPECT_EQ(*(v[2]), "test_3");
+    v.clear();
+}
