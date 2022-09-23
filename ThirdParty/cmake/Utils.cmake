6ee0abc40473d549e171cd2c1a852731834feb53
Philip Top <phlptp@gmail.com>
2022-09-22 19:48:35 -0700

    move the thirdparty cmake stuff to separate folder

--- ThirdParty/cmake/Utils.cmake
+++ ThirdParty/cmake/Utils.cmake
@@ -9,15 +9,9 @@ function(xor result)
     endforeach()
 
     if(NOT (${true_args_count} EQUAL 1))
-        set(${result}
-            FALSE
-            PARENT_SCOPE
-        )
+        set(${result} FALSE PARENT_SCOPE)
     else()
-        set(${result}
-            TRUE
-            PARENT_SCOPE
-        )
+        set(${result} TRUE PARENT_SCOPE)
     endif()
 endfunction()
 
@@ -31,14 +25,8 @@ function(at_most_one result)
     endforeach()
 
     if(${true_args_count} GREATER 1)
-        set(${result}
-            FALSE
-            PARENT_SCOPE
-        )
+        set(${result} FALSE PARENT_SCOPE)
     else()
-        set(${result}
-            TRUE
-            PARENT_SCOPE
-        )
+        set(${result} TRUE PARENT_SCOPE)
     endif()
 endfunction()
