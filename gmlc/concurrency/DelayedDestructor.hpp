b2b7865917cac516bdfa0c843b61d1a75176c730
Philip Top <phlptp@gmail.com>
2023-05-11 13:28:36 -0700

    Delayed destructor single thread (#9)
    * add singethread Delayed Destructor
    
    * add single thread delayed destructor and some very basic tests and a fix of some incorrect assumptions
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update CPPLINT config
    
    * use gcc13 builder due to tsan false positive issue in gcc12
    
    * add suppression
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- gmlc/concurrency/DelayedDestructor.hpp
+++ gmlc/concurrency/DelayedDestructor.hpp
@@ -89,27 +89,28 @@ class DelayedDestructor {
             elementSize = ElementsToBeDestroyed.size();
             if (elementSize > 0) {
                 std::vector<std::shared_ptr<X>> ecall;
-                std::vector<std::string> ename;
+                std::vector<void*> epointers;
                 for (auto& element : ElementsToBeDestroyed) {
                     if (element.use_count() == 1) {
                         ecall.push_back(element);
-                        ename.push_back(element->getIdentifier());
+                        epointers.emplace_back(element.get());
                     }
                 }
-                if (!ename.empty()) {
+                if (!epointers.empty()) {
                     // so apparently remove_if can actually call the
                     // destructor for shared_ptrs so the call function needs
                     // to be before this call
                     auto loc = std::remove_if(
                         ElementsToBeDestroyed.begin(),
                         ElementsToBeDestroyed.end(),
-                        [&ename](const auto& element) {
+                        [&epointers](const auto& element) {
                             return (
                                 (element.use_count() == 2) &&
                                 (std::find(
-                                     ename.begin(),
-                                     ename.end(),
-                                     element->getIdentifier()) != ename.end()));
+                                     epointers.begin(),
+                                     epointers.end(),
+                                     static_cast<void*>(element.get())) !=
+                                 epointers.end()));
                         });
                     ElementsToBeDestroyed.erase(
                         loc, ElementsToBeDestroyed.end());
@@ -176,10 +177,165 @@ class DelayedDestructor {
         }
         return ElementsToBeDestroyed.size();
     }
+    /// @brief  get the number of elements waiting to be destroyed
+    /// @return number of objects
+    auto size()
+    {
+        std::lock_guard<std::timed_mutex> lock(destructionLock);
+        return ElementsToBeDestroyed.size();
+    }
 
     void addObjectsToBeDestroyed(std::shared_ptr<X> obj)
     {
         std::lock_guard<std::timed_mutex> lock(destructionLock);
+        ElementsToBeDestroyed.push_back(std::move(obj));
+    }
+};
+
+/// @brief  handle the delayed destructor as a single thread so no locks,
+/// possible use with thread local structure
+/// @tparam X the class of object to be destroyed
+template<class X>
+class DelayedDestructorSingleThread {
+  private:
+    std::vector<std::shared_ptr<X>> ElementsToBeDestroyed;
+    std::function<void(std::shared_ptr<X>& ptr)> callBeforeDeleteFunction;
+#ifdef ENABLE_TRIPWIRE
+    TripWireDetector tripDetect;
+#endif
+
+  public:
+    DelayedDestructorSingleThread() = default;
+    explicit DelayedDestructorSingleThread(
+        std::function<void(std::shared_ptr<X>& ptr)> callFirst) :
+        callBeforeDeleteFunction(std::move(callFirst))
+    {
+    }
+    ~DelayedDestructorSingleThread()
+    {
+        try {
+            int ii = 0;
+            while (!ElementsToBeDestroyed.empty()) {
+                ++ii;
+                destroyObjects();
+                if (!ElementsToBeDestroyed.empty()) {
+#ifdef ENABLE_TRIPWIRE
+                    // short circuit if the tripline was triggered
+                    if (tripDetect.isTripped()) {
+                        return;
+                    }
+#endif
+                    if (ii > 4) {
+                        destroyObjects();
+                        break;
+                    }
+                    if (ii % 2 == 0) {
+                        std::this_thread::sleep_for(
+                            std::chrono::milliseconds(100));
+                    } else {
+                        std::this_thread::yield();
+                    }
+                }
+            }
+        }
+        catch (...) {
+        }
+    }
+    DelayedDestructorSingleThread(DelayedDestructorSingleThread&&) noexcept =
+        delete;
+    DelayedDestructorSingleThread&
+        operator=(DelayedDestructorSingleThread&&) noexcept = delete;
+
+    /** destroy objects that are no longer used*/
+    size_t destroyObjects() noexcept
+    {
+        std::size_t elementSize{static_cast<std::size_t>(-1)};
+        std::chrono::milliseconds wait(std::chrono::milliseconds(50));
+        try {
+            elementSize = ElementsToBeDestroyed.size();
+            if (elementSize > 0) {
+                std::vector<std::shared_ptr<X>> ecall;
+                std::vector<void*> epointers;
+                for (auto& element : ElementsToBeDestroyed) {
+                    if (element.use_count() == 1) {
+                        ecall.push_back(element);
+                        epointers.emplace_back(element.get());
+                    }
+                }
+                if (!epointers.empty()) {
+                    // so apparently remove_if can actually call the
+                    // destructor for shared_ptrs so the call function needs
+                    // to be before this call
+                    auto loc = std::remove_if(
+                        ElementsToBeDestroyed.begin(),
+                        ElementsToBeDestroyed.end(),
+                        [&epointers](const auto& element) {
+                            return (
+                                (element.use_count() == 2) &&
+                                (std::find(
+                                     epointers.begin(),
+                                     epointers.end(),
+                                     static_cast<void*>(element.get())) !=
+                                 epointers.end()));
+                        });
+                    ElementsToBeDestroyed.erase(
+                        loc, ElementsToBeDestroyed.end());
+                    elementSize = ElementsToBeDestroyed.size();
+                    auto deleteFunc = callBeforeDeleteFunction;
+
+                    // this needs to be done after the lock, so a destructor
+                    // can never called while under the lock
+                    if (deleteFunc) {
+                        for (auto& element : ecall) {
+                            deleteFunc(element);
+                        }
+                    }
+                    ecall.clear();  // make sure the destructors get called
+                    // before returning.
+                }
+            }
+            return ElementsToBeDestroyed.size();
+        }
+        catch (...) {
+        }
+        return elementSize;
+    }
+
+    size_t destroyObjects(std::chrono::milliseconds delay)
+    {
+        using namespace std::literals::chrono_literals;
+
+        auto delayTime = (delay < 100ms) ? delay : 50ms;
+        int delayCount =
+            (delay < 100ms) ? 1 : static_cast<int>((delay.count() / 50));
+
+        int cnt = 0;
+        auto elementSize = ElementsToBeDestroyed.size();
+        while (elementSize > 0 && (cnt < delayCount)) {
+            if (cnt > 0)  // don't sleep on the first loop
+            {
+                if (delay > 4ms) {
+                    std::this_thread::sleep_for(delayTime);
+                }
+            }
+            ++cnt;
+            elementSize = ElementsToBeDestroyed.size();
+            if (elementSize > 0) {
+                destroyObjects();
+            }
+        }
+        return ElementsToBeDestroyed.size();
+    }
+
+    /// @brief  get the number of elements waiting to be destroyed
+    /// @return number of objects
+    auto size()
+    {
+        return ElementsToBeDestroyed.size();
+    }
+
+    void addObjectsToBeDestroyed(std::shared_ptr<X> obj)
+    {
         ElementsToBeDestroyed.push_back(std::move(obj));
     }
 };
---
+++ tests/DelayedDestructorTests.cpp
@@ -0,0 +1,44 @@
+/*
+Copyright (c) 2017-2022,
+Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance
+for Sustainable Energy, LLC.  See the top-level NOTICE for additional details.
+All rights reserved. SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#include <future>
+#include <memory>
+#include <string>
+#include <thread>
+#include <utility>
+/** these test cases test data_block and data_view objects
+ */
+
+#include "concurrency/DelayedDestructor.hpp"
+
+#include "gtest/gtest.h"
+#include <iostream>
+
+using namespace gmlc::concurrency;
+
+/** test basic operations */
+TEST(DelayedDestr, basic)
+{
+    DelayedDestructor<std::string> DD1;
+    DD1.addObjectsToBeDestroyed(std::make_shared<std::string>("test_1"));
+
+    EXPECT_EQ(DD1.size(), 1U);
+
+    DD1.destroyObjects();
+    EXPECT_EQ(DD1.size(), 0U);
+}
+
+TEST(DelayedDestrSS, basic)
+{
+    DelayedDestructorSingleThread<std::string> DD1;
+    DD1.addObjectsToBeDestroyed(std::make_shared<std::string>("test_1"));
+
+    EXPECT_EQ(DD1.size(), 1U);
+
+    DD1.destroyObjects();
+    EXPECT_EQ(DD1.size(), 0U);
+}
