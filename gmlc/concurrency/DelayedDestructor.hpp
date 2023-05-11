36c57bced67f89c76c249677c14ddbf6c5e010ae
Philip Top <top1@llnl.gov>
2023-05-11 10:24:36 -0700

    add singethread Delayed Destructor

--- gmlc/concurrency/DelayedDestructor.hpp
+++ gmlc/concurrency/DelayedDestructor.hpp
@@ -183,4 +183,141 @@ class DelayedDestructor {
     }
 };
 
+/// @brief  handle the delayed destructor as a single thread so no locks, possible use with thread local structure
+/// @tparam X the class of object to be destroyed
+template<class X>
+class DelayedDestructorSingleThread {
+private:
+    std::vector<std::shared_ptr<X>> ElementsToBeDestroyed;
+    std::function<void(std::shared_ptr<X>& ptr)> callBeforeDeleteFunction;
+#ifdef ENABLE_TRIPWIRE
+    TripWireDetector tripDetect;
+#endif
+
+public:
+    DelayedDestructorSingleThread() = default;
+    explicit DelayedDestructorSingleThread(
+        std::function<void(std::shared_ptr<X>& ptr)> callFirst) :
+        callBeforeDeleteFunction(std::move(callFirst))
+    {
+    }
+    ~DelayedDestructorSingleThread()
+    {
+        try {
+            int ii = 0;
+            while (!ElementsToBeDestroyed.empty()) {
+                ++ii;
+                destroyObjects();
+                if (!ElementsToBeDestroyed.empty()) {
+#ifdef ENABLE_TRIPWIRE
+                    // short circuit if the tripline was triggered
+                    if (tripDetect.isTripped()) {
+                        return;
+                    }
+#endif
+                    if (ii > 4) {
+                        destroyObjects();
+                        break;
+                    }
+                    if (ii % 2 == 0) {
+                        std::this_thread::sleep_for(
+                            std::chrono::milliseconds(100));
+                    } else {
+                        std::this_thread::yield();
+                    }
+                }
+            }
+        }
+        catch (...) {
+        }
+    }
+    DelayedDestructorSingleThread(DelayedDestructorSingleThread&&) noexcept = delete;
+    DelayedDestructorSingleThread& operator=(DelayedDestructorSingleThread&&) noexcept = delete;
+
+    /** destroy objects that are no longer used*/
+    size_t destroyObjects() noexcept
+    {
+        std::size_t elementSize{static_cast<std::size_t>(-1)};
+        std::chrono::milliseconds wait(std::chrono::milliseconds(50));
+        try {
+            elementSize = ElementsToBeDestroyed.size();
+            if (elementSize > 0) {
+                std::vector<std::shared_ptr<X>> ecall;
+                std::vector<std::string> ename;
+                for (auto& element : ElementsToBeDestroyed) {
+                    if (element.use_count() == 1) {
+                        ecall.push_back(element);
+                        ename.push_back(element->getIdentifier());
+                    }
+                }
+                if (!ename.empty()) {
+                    // so apparently remove_if can actually call the
+                    // destructor for shared_ptrs so the call function needs
+                    // to be before this call
+                    auto loc = std::remove_if(
+                        ElementsToBeDestroyed.begin(),
+                        ElementsToBeDestroyed.end(),
+                        [&ename](const auto& element) {
+                            return (
+                                (element.use_count() == 2) &&
+                                (std::find(
+                                    ename.begin(),
+                                    ename.end(),
+                                    element->getIdentifier()) != ename.end()));
+                        });
+                    ElementsToBeDestroyed.erase(
+                        loc, ElementsToBeDestroyed.end());
+                    elementSize = ElementsToBeDestroyed.size();
+                    auto deleteFunc = callBeforeDeleteFunction;
+
+                    // this needs to be done after the lock, so a destructor
+                    // can never called while under the lock
+                    if (deleteFunc) {
+                        for (auto& element : ecall) {
+                            deleteFunc(element);
+                        }
+                    }
+                    ecall.clear();  // make sure the destructors get called
+                    // before returning.
+                }
+            }
+            return ElementsToBeDestroyed.size();
+        }
+        catch (...) {
+        }
+        return elementSize;
+    }
+
+    size_t destroyObjects(std::chrono::milliseconds delay)
+    {
+        using namespace std::literals::chrono_literals;
+       
+        auto delayTime = (delay < 100ms) ? delay : 50ms;
+        int delayCount =
+            (delay < 100ms) ? 1 : static_cast<int>((delay.count() / 50));
+
+        int cnt = 0;
+        auto elementSize = ElementsToBeDestroyed.size();
+        while (elementSize > 0 && (cnt < delayCount)) {
+            if (cnt > 0)  // don't sleep on the first loop
+            {
+                if (delay > 4ms) {
+                    std::this_thread::sleep_for(delayTime);
+                }
+            }
+            ++cnt;
+            elementSize = ElementsToBeDestroyed.size();
+            if (elementSize > 0) {
+                destroyObjects();
+            }
+        }
+        return ElementsToBeDestroyed.size();
+    }
+
+    void addObjectsToBeDestroyed(std::shared_ptr<X> obj)
+    {
+        ElementsToBeDestroyed.push_back(std::move(obj));
+    }
+};
+
 }  // namespace gmlc::concurrency
---
+++ tests/DelayedDestructorTests.cpp
@@ -0,0 +1,71 @@
+/*
+Copyright (c) 2017-2022,
+Battelle Memorial Institute; Lawrence Livermore National Security, LLC; Alliance
+for Sustainable Energy, LLC.  See the top-level NOTICE for additional details.
+All rights reserved. SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#include <future>
+#include <memory>
+#include <string>
+#include <thread>
+#include <utility>
+/** these test cases test data_block and data_view objects
+ */
+
+#include "concurrency/SearchableObjectHolder.hpp"
+
+#include "gtest/gtest.h"
+#include <iostream>
+
+using namespace gmlc::concurrency;
+
+/** test basic operations */
+TEST(SOH, basic)
+{
+    SearchableObjectHolder<std::string> SOH1;
+    SOH1.addObject("test1", std::make_shared<std::string>("test_1"));
+
+    auto res = SOH1.findObject("test1");
+    ASSERT_TRUE(res);
+    EXPECT_EQ(*res, "test_1");
+
+    SOH1.removeObject("test1");
+    auto res2 = SOH1.findObject("test1");
+    EXPECT_FALSE(res2);
+}
+
+TEST(SOH, contained)
+{
+    SearchableObjectHolder<std::string> SOH1;
+    EXPECT_TRUE(SOH1.empty());
+    SOH1.addObject("test1", std::make_shared<std::string>("test_1"));
+    SOH1.addObject("test2", std::make_shared<std::string>("test_2"));
+    SOH1.addObject("test3", std::make_shared<std::string>("test_3"));
+
+    auto objects = SOH1.getObjects();
+    ASSERT_EQ(objects.size(), 3U);
+    EXPECT_EQ(*(objects[0]), "test_1");
+    EXPECT_EQ(*(objects[1]), "test_2");
+    EXPECT_EQ(*(objects[2]), "test_3");
+    objects.clear();
+}
+
+TEST(SOH, containedType)
+{
+    SearchableObjectHolder<std::string, char> SOH1;
+    EXPECT_TRUE(SOH1.empty());
+    SOH1.addObject("test1", std::make_shared<std::string>("test_1"), '1');
+    SOH1.addObject("test2", std::make_shared<std::string>("test_2"), '2');
+    SOH1.addObject("test3", std::make_shared<std::string>("test_3"), '3');
+
+    auto objects = SOH1.getObjects();
+    ASSERT_EQ(objects.size(), 3U);
+    EXPECT_EQ(*(objects[0]), "test_1");
+    EXPECT_EQ(*(objects[1]), "test_2");
+    EXPECT_EQ(*(objects[2]), "test_3");
+
+    EXPECT_TRUE(SOH1.checkObjectType("test1", '1'));
+
+    objects.clear();
+}
